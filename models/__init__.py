218da90c58212d0f2736cff1e9efd6c67af4d1ec
frfravani <30913514+frfravani@users.noreply.github.com>
2026-05-05 12:31:42 -0300

    Add models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from models.author import Author
+from models.book import Book
+from models.magazine import Magazine
+from models.conversation import Conversation
+from models.user import User
+
+__all__ = ["Author", "Book", "Magazine", "Conversation", "User"]
